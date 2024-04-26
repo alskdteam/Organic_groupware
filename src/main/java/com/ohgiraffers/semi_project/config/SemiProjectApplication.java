d9825ad28044ae219c249db18dc8e5e380295cf7
pyunghun <564564q@naver.com>
2024-04-26 20:41:34 +0900

    security

--- src/main/java/com/ohgiraffers/semi_project/config/SemiProjectApplication.java
+++ src/main/java/com/ohgiraffers/semi_project/config/SemiProjectApplication.java
@@ -1,4 +1,4 @@
-package com.ohgiraffers.semi_project;
+package com.ohgiraffers.semi_project.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SemiProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SemiProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SemiProjectApplication.class, args);
+    }
 
 }
