65af5a5c19d15d18f3e6977c4b491fe27aac91a8
gyuhyeok <153148788+gyuhyeok0@users.noreply.github.com>
2024-04-26 21:16:54 +0900

    Update SemiProjectApplication.java

--- src/main/java/com/ohgiraffers/semi_project/config/SemiProjectApplication.java
+++ src/main/java/com/ohgiraffers/semi_project/config/SemiProjectApplication.java
@@ -1,13 +1,1 @@
-package com.ohgiraffers.semi_project.config;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SemiProjectApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SemiProjectApplication.class, args);
-    }
-
-}
