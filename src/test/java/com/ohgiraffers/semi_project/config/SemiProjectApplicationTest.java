bb908c22a3479e23864b2592593cd99298550cf9
rjadmsehf <jjg524@naver.com>
2024-06-22 17:04:03 +0900

    test: SemiProjectApplicationTest 테스트 추가

---
+++ src/test/java/com/ohgiraffers/semi_project/config/SemiProjectApplicationTest.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.semi_project.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SemiProjectApplicationTest {
+    @Test
+    public void initTest() {
+        System.out.println("success");
+    }
+}
